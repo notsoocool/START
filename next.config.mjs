4ecdb1c1b2487d71e49cef62c653ee15ebf2a1ce
Yajush Vyas <dryashhoney@gmail.com>
2026-03-02 16:06:28 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	// Reduce bundle size: tree-shake lucide-react, @radix-ui, lodash
-	experimental: {
-		optimizePackageImports: [
-			"lucide-react",
-			"@radix-ui/react-icons",
-			"@radix-ui/react-dialog",
-			"@radix-ui/react-dropdown-menu",
-			"@radix-ui/react-popover",
-			"@radix-ui/react-select",
-			"@radix-ui/react-tabs",
-			"@radix-ui/react-tooltip",
-		],
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
